421f5d656ca379b13ca7d1ad1f708b2338deb341
R. Shinken <95217423+bnf2462@users.noreply.github.com>
2021-12-14 16:57:06 +0900

    Add files via upload

---
+++ L5. Numbers and Integer Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // How to use int. (ST:2021/12/14 16:40)
+            int a = 60;
+            int b = 30;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            // caculate in different way. (ET:2021/12/14 16:50)
+            int d = a * b;
+            Console.WriteLine(d);
+
+            d = a / b;
+            Console.WriteLine(d);
+
+            d = a * (b + 20) / 2 * 15 + c;
+            Console.WriteLine(d);
+
+            Console.ReadKey();
+        }
+    }
+}
